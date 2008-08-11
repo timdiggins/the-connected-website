586d5812d3337a99a2a48f6242a9b0205a0f0f48
Duff OMelia <duff@omelia.org>
2008-08-11 19:10:50 -0400

    Now showing truncated detail of posts on post index page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,16 @@ class Post < ActiveRecord::Base
   
   named_scope :sorted_by_created, lambda { { :order => "updated_at DESC" }}
   
+  def brief
+    truncate(detail, 200)
+  end
+
+  private
+    def truncate(text, length)
+      truncate_string = "..."
+      l = length - truncate_string.chars.length
+      (text.chars.length > length ? text.chars[0...l] + truncate_string : text).to_s
+    end
+  
   
 end
