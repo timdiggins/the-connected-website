77b4b05d263861db50a0176c908733664fae87f5
Duff OMelia <duff@omelia.org>
2008-08-11 18:23:39 -0400

    Now auto linking urls.  Now auto-embedding images.  Now redirecting to the post after it's created.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  
+  def detail_format(detail)
+    stripped = h(detail)
+    linked = auto_link(stripped) { | each | each[/\.(jpg|gif|png)$/] ? %Q{<img src="#{each}" width="160px" class="embeddedImage" />} : each }
+    simple_format(linked)
+  end
+  
 end
