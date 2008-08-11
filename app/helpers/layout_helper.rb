56477c079f6909ffc6531f588ebf57a5dd2dc361
Duff OMelia <duff@omelia.org>
2008-08-11 08:41:47 -0400

    Now using a default layout.

---
+++ app/helpers/layout_helper.rb
@@ -0,0 +1,17 @@
+module LayoutHelper
+  
+  def flash_class
+    return "" if flash.values.empty?
+    %{class = "#{flash.keys.join(' ')}"}
+  end
+  
+  def flash_value
+    flash.values.collect { | value | "<p>#{value}</p>"}.join("\n")
+  end
+  
+  def body_tag
+    onload_str = @onload_event ? %{ onload="#{@onload_event}"} : ''
+    "<body#{onload_str}>"
+	end
+  
+end
