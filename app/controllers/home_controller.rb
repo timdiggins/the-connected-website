d794bd72965d97dd70d4d97e4c2ec49ad5914935
Tim Diggins <tim@red56.co.uk>
2008-08-30 00:55:04 +0100

    Improving output of warnings that you aren't going to be listed on People page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.featured 
-    if @posts.size <1 
+    if @posts.size < 1 
       @posts = Post.sorted_by_updated_at
     end
     
