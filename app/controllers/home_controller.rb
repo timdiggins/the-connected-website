37c98df55369bb39f4de0f7fdba0e598dcfc4f47
Tim Diggins <tim@red56.co.uk>
2008-08-30 00:44:08 +0100

    made home page use featured (if there is at least one featured, otherwise it just uses recent)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.sorted_by_updated_at
+    @posts = Post.featured 
+    if @posts.size <1 
+      @posts = Post.sorted_by_updated_at
+    end
+    
     @events = Event.sorted_by_created_at
     @users = User.some_having_bio_and_avatar
     @post = @posts[0]
