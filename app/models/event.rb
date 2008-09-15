81925b043fef118e72b311194f404557ee288e24
Duff OMelia <duff@omelia.org>
2008-09-15 18:34:31 -0400

    Now only showing 15 most recent events on the home page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ActiveRecord::Base
   belongs_to :detail, :polymorphic => true
   
   named_scope :sorted_by_created_at, lambda { { :order => "created_at DESC" }}  
+  named_scope :limit_to, lambda { | number | { :limit => number } }
   
   def self.create_for(detail)
     event = Event.new(:user => detail.user)
