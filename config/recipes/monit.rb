5f6816fba8b92492e17de53bf82f657c2f8a44be
Michael Hale <mikehale@gmail.com>
2008-09-24 12:31:32 -0400

    hopefully make monit and mongrel play nicer

--- config/recipes/monit.rb
+++ config/recipes/monit.rb
@@ -4,31 +4,40 @@ Capistrano::Configuration.instance.load do
   namespace :deploy do
     desc "restart mongrels"
     task :restart do
-      run "sudo #{monit} unmonitor all -g #{application}_mongrels"
-      run "sudo /usr/bin/mongrel_rails cluster::stop -C /etc/mongrel_cluster/#{application}.yml --clean"
-      run "sudo /usr/bin/mongrel_rails cluster::start -C /etc/mongrel_cluster/#{application}.yml --clean"
-      run "sudo #{monit} monitor all -g #{application}_mongrels"
+      stop
+      start
     end
     
     desc "stop mongrels"
     task :stop do
       run "sudo #{monit} unmonitor all -g #{application}_mongrels"
-      run "sudo /usr/bin/mongrel_rails cluster::stop -C /etc/mongrel_cluster/#{application}.yml --clean"
+      sleep 0.5
+      mongrel.stop
     end
     
     desc "start mongrels"
     task :start do
-      run "sudo /usr/bin/mongrel_rails cluster::start -C /etc/mongrel_cluster/#{application}.yml --clean"
+      mongrel.start
       run "sudo #{monit} monitor all -g #{application}_mongrels"
-    end    
-    
-    namespace :web do
-      %w(start stop restart).each {|action|
-        desc "#{action} nginx"
-        task action.to_sym do
-          run "sudo #{monit} #{action} all -g www"
-        end
-      }      
-    end
+    end        
   end
+  
+  namespace :web do
+    %w(start stop restart).each {|action|
+      desc "#{action} webserver"
+      task action.to_sym do
+        run "sudo #{monit} #{action} all -g www"
+      end
+    }      
+  end
+  
+  namespace :mongrel do
+    %w(start stop restart).each {|action|
+      desc "#{action} mongrels"
+      task action.to_sym do
+        run "sudo /usr/bin/mongrel_rails cluster::#{action} -C /etc/mongrel_cluster/#{application}.yml --clean"
+      end
+    }    
+  end
+  
 end
