ac3c3d76e928ec4e04a2a103d44decf2a5bf7e99
Duff OMelia <duff@omelia.org>
2008-09-24 12:26:17 -0400

    Added RSS feed for all posts.  Added RSS feed for featuerd posts.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,17 @@ module PostsHelper
     prefix = (post.created_at == post.updated_at) ? "created" : "last updated"
     "#{prefix} #{time_ago_in_words(post.updated_at)} ago"
   end
+  
+  def render_posts(xml, posts)
+    for post in posts
+      xml.item do
+        xml.title post.title
+        xml.description post.detail
+        xml.pubDate post.created_at.to_s(:rfc822)
+        xml.link post_url(post)
+        xml.author(post.user)               
+        xml.guid(post_url(post))
+      end
+    end
+  end
 end
