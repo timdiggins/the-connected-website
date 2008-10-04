b42ea0e0f679d5746e7c6a995b41b427bc871d54
Duff OMelia <duff@omelia.org>
2008-10-04 00:55:52 -0400

    Refactored tests a bit.  Hope Tim is ok with it. :)

--- test/integration/user_page_test.rb
+++ test/integration/user_page_test.rb
@@ -5,33 +5,14 @@ class UserPageTest < ActionController::IntegrationTest
 
   should "user page with no postings should be as expected" do
     get("users/duff")
-    assert_has_selector_containing_text("h2", /.*Recently.*/, {:exactly => 0 })
-    assert_has_selector_containing_text("h2", /.*recent posts.*/, {:exactly => 0 })
+    assert_select('h2', :text => /Recently/, :count => 0)
+    assert_select('h2', :text => /recent posts/, :count => 0)
   end
 
   should "user page with postings should be as expected" do
-    print "user page with postings should be as expected"
     get("users/alex")
-    assert_has_selector_containing_text("h2", /.*Recently.*/, {:exactly => 1 })
-    assert_has_selector_containing_text("h2", /.*recent posts.*/, {:exactly => 1 })
-  end
-
-  def assert_has_selector_containing_text(selector, textMatcher, count)
-    puts selector
-    found = 0
-    elems = css_select(selector)
-      elems.each { |elem |
-      puts "aha",  elem.class, elem
-      if elem.to_s =~ textMatcher
-        puts "elem matches"
-        found += 1
-      end
-    }
-    puts found
-    if count.has_key?(:exactly)
-      assert_equal count[:exactly], found
-    end
-    
+    assert_select('h2', /Recently/)
+    assert_select('h2', /recent posts/)
   end
 
 end
