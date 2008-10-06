d7e5fcda81754092960f96c5e1dce3f2e92f1f26
Duff OMelia <duff@omelia.org>
2008-10-06 17:09:06 -0400

    Trying to handle weird stuff in the tag urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users, :member => { :become => :post }, :collection => { :all => :get }
-  map.resources :sessions
-  map.resources :posts, :has_many => [ :comments, :tags ], :member => { :feature => :put, :unfeature => :put }, :collection => { :featured => :get }
-  map.resources :tags
-  map.resource  :settings, :collection => { :save_new_avatar => :put, :picture => :get, :username_email => :any, :bio => :any, :password => :any }
-
   map.login '/login', :controller => "sessions", :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.forgot_password '/login/forgot_password', :controller => 'sessions', :action => 'forgot_password'
   map.reset_password 'rp/:reset_password_token', :controller => "sessions", :action => 'reset_password'
   map.admin_emails 'admin/emails', :controller => 'admin', :action => 'emails'
-  
   map.info 'info/:action', :controller => 'info'
+
+  map.resources :users, :member => { :become => :post }, :collection => { :all => :get }
+  map.resources :sessions
+  map.resources :posts, :has_many => [ :comments, :tags ], :member => { :feature => :put, :unfeature => :put }, :collection => { :featured => :get }
+  map.resources :tags, :requirements => { :id => /.*/ }
+  map.resource  :settings, :collection => { :save_new_avatar => :put, :picture => :get, :username_email => :any, :bio => :any, :password => :any }
   
+
   map.root :controller => "home"
   
-  # If the user has typed an Invalid URL
   map.connect "*path", :controller => "www", :action => "unrecognized404"
 end
 
