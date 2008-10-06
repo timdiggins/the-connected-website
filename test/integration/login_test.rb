cd74b4721d5d99fd22da438ad39e0a86bf82c73a
Duff OMelia <duff@omelia.org>
2008-10-06 15:09:37 -0400

    Fixed closing paragraph tag

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -10,11 +10,10 @@ class LoginTest < ActionController::IntegrationTest
 
   should "handle valid login" do
     login(:duff)
-    
     assert_select("p", {:text => "Invalid email or password.", :count => 0 })
     assert_flash("Logged in successfully")
     assert_logged_in(:duff)    
-
+      
     alex_session = new_session_as :alex
     assert_logged_in(:duff)    
     alex_session.assert_logged_in(:alex)
