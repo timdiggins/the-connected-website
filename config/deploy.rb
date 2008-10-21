a6910ebb34f9e6201d9af1ebe447a746d1adcacd
Duff OMelia <duff@omelia.org>
2008-10-21 08:57:38 -0400

    Moved monit recipes back.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ require 'capistrano/ext/multistage'
 require 'spacesuit/recipes/multistage_patch'
 require 'spacesuit/recipes/common'
 require 'mongrel_cluster/recipes_2'
+require 'config/recipes/monit'
 
 set :application, "republic"
 set :rails_env, "production"
