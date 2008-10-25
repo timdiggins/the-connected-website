691ea7f2ccaa00e60083eeaa2e973913405f1133
Duff OMelia <duff@omelia.org>
2008-10-24 21:46:31 -0400

    Now testing some tagging functionality.

---
+++ test/integration/tag_test.rb
@@ -0,0 +1,78 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+
+class TagTest < ActionController::IntegrationTest
+
+  should "show no tags for the post" do
+    post_id = posts(:cool_article).id
+    get "/posts/#{post_id}"
+    assert_select "div.tags>ul>li", :count => 0
+    assert_select 'label', :text => /Add item to a tag/, :count => 0
+  end
+
+  should "be able to add tags to a post" do
+    new_session_as(:duff) do
+      post_id = posts(:cool_article).id
+      get "/posts/#{post_id}"
+      assert_select 'label', :text => /Add item to a tag/
+      
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Lame Government"
+      assert_select "div.tags>ul>li>a", "Lame Government"
+      assert_select 'label', :text => /Add item to another tag/
+      assert_select 'label', :text => /Add item to a tag/, :count => 0
+      
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Craziness"
+      assert_select "div.tags>ul>li", :count => 2
+    end
+  end
+  
+  should "handle the case of adding the same tag again" do
+    new_session_as(:duff) do
+      post_id = posts(:cool_article).id
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Lame Government"
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "LAME Government"
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "CraZinesS"
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Craziness"
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Craziness"
+      assert_select "div.tags>ul>li", :count => 2
+      assert_select "div.tags>ul>li>a", "Lame Government"
+      assert_select "div.tags>ul>li>a", "CraZinesS"
+    end
+  end
+
+  
+  should "be able to click a tag name and see the posts with that tag" do
+    new_session_as(:duff) do
+      post_id = posts(:cool_article).id
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Lame Government"
+      click_link "Lame Government"
+      assert_select "h1", /Lame Government 1 article/
+      assert_select "div.posts>div.post", :count => 1
+      assert_select "div.posts>div.post>div.postContent>h2>a", "Government is Bogus"
+    end
+  end
+  
+  should "see the tags of a post on the home page and the postings page" do
+    new_session_as(:duff) do
+      post_id = posts(:cool_article).id
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Lame Government"
+      get '/'
+      assert_select "p.tags>a", "Lame Government"
+      
+      get '/posts'
+      assert_select "p.tags>a", "Lame Government"
+    end
+  end
+  
+  should "see the tagging event" do
+    new_session_as(:duff) do
+      post_id = posts(:cool_article).id
+      post_via_redirect "posts/#{post_id}/tags", :tag_name => "Lame Government"
+      get '/'
+      assert_select "div.eventBody>p.details", /duff tagged Government is Bogus.*with the tag Lame Government.*less than a minute ago/m
+      assert_select "div.eventBody>p.details>a", "duff"
+      assert_select "div.eventBody>p.details>a", "Government is Bogus"
+      assert_select "div.eventBody>p.details>a", "Lame Government"
+    end
+  end
+end
+
