8f662527631b2bb1835dc5fc24887f59a0ffe68d
Duff OMelia <duff@omelia.org>
2008-11-10 15:24:12 -0500

    Now hiding tagging events on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @featured = Post.sorted_by_updated_at.limit_to(6) if @featured.empty?
     @current_featured = @featured.shift
     
-    @events = Event.sorted_by_created_at.limit_to(15)
+    @events = Event.sorted_by_created_at.without_tagging_events.limit_to(15)
     @users = User.recently_contributed(3)
   end
 end
