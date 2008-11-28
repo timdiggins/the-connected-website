2a1e574153cb75a3ebda460a5654722948c86e5f
Tim Diggins <tim@red56.co.uk>
2008-11-28 15:09:47 +0000

    some slight additions (headlines, and latest event)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @current_featured = @featured.shift
     
     @events = Event.sorted_by_created_at.without_tagging_events.limit_to(5)
+    @latest_event = Event.sorted_by_created_at.first
     @users = User.recently_contributed(3)
     @tags = Tag.all_with_count :limit=>20
   end
