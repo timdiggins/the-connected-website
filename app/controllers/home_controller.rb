529a4eed4636267abe49a635db7e6b6b681b9869
Tim Diggins <tim@red56.co.uk>
2008-12-18 12:09:21 +0000

    make sure people with no avatar don't appear in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
     
     @events = Event.sorted_by_created_at.without_tagging_events.limit_to(5)
     @latest_event = Event.sorted_by_created_at.first
-    @users = User.recently_contributed(3)
+    @users = User.has_bio_and_avatar.recently_contributed(3)
     @tags = Tag.all_with_count :limit=>20
   end
 end
