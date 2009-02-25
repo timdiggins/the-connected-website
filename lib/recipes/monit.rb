9344ea6c8a8b2a900ae28720e55907bd0de33650
Michael Hale <mikehale@gmail.com>
2009-02-24 19:55:38 -0500

    run daemons as app user

--- lib/recipes/monit.rb
+++ lib/recipes/monit.rb
@@ -2,7 +2,7 @@ set :monit_path, "/usr/local/bin/monit"
 namespace :monit do
   %w(summary status).each do |action|
     desc "run monit #{action}"
-    task action.to_sym  do
+    task action.to_sym do
       puts capture("sudo #{monit_path} #{action}")
     end
   end
