1bc82667b86a57637b7385261ee00f384cc4f98f
Michael Hale <mikehale@gmail.com>
2009-02-25 11:53:19 -0500

    update gem requirement

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,6 @@ Rails::Initializer.run do |config|
   config.gem "redgreen", :version => '1.2.2' unless ENV['TM_MODE']
   config.gem "map_by_method", :version => '0.8.3'
   config.gem "colored", :version => '1.1'
-  config.gem "mikehale-daemons", :lib => "daemons", :version => '1.0.12.1', :source => "http://gems.github.com"
+  config.gem "mikehale-daemons", :lib => "daemons", :version => '>=1.0.12', :source => "http://gems.github.com"
   
 end
