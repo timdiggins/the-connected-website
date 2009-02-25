d2fd65825b1f4da0fbd50d57ede22e74cc8a18a8
Michael Hale <mikehale@gmail.com>
2009-02-24 22:37:01 -0500

    use mikehale-daemons

--- lib/daemons_patch.rb
+++ lib/daemons_patch.rb
@@ -1,32 +1,4 @@
-# Optionally specify the logdir seperately from the piddir
 class Daemons::Application
-  def logdir
-    logdir = options[:log_dir]
-    unless logdir
-      logdir = options[:dir_mode] == :system ? '/var/log' : pidfile_dir
-    end
-    logdir
-  end
-
-  def output_logfile
-    (options[:log_output] && logdir) ? File.join(logdir, @group.app_name + '.output') : nil
-  end
-
-  def logfile
-    logdir ? File.join(logdir, @group.app_name + '.log') : nil
-  end
-end
-
-require File.join(File.dirname(__FILE__), 'change_privilege')
-class Daemons::Application
-  alias :old_initialize :initialize 
-  class_eval %{
-    def initialize(*args, &block)
-      old_initialize(*args, &block)
-      change_privilege
-    end
-  }
-
   def change_privilege
     environment_stat = File.stat(File.join(File.dirname(__FILE__), '..', 'config', 'environment.rb'))
     user = Etc.username(environment_stat.uid)
