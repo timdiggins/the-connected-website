68ae0668997131fc389d2a478660ad308ccbf6dd
Tim Diggins <tim@red56.co.uk>
2009-03-06 16:38:22 +0000

    fixed image_tag_for to use image width and height where exists

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,15 @@
 class PostImage < ActiveRecord::Base
   belongs_to :post
-
+  
   named_scope :latest3, :limit=>3, :order => "updated_at DESC"
   named_scope :featured
+  
+  def width_for_height(h)
+    return nil if self.width.nil? || self.height.nil? 
+    h*self.width/self.height
+  end
+  def height_for_width(w)
+    return nil if self.width.nil? || self.height.nil?
+    w*self.height/self.width
+  end
 end
