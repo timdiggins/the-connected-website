126dd199a95c58e2f5b55aa2801e784fe93b74da
Alexander Kohlhofer <alexander@kohlhofer.com>
2009-03-06 17:13:09 +0100

    all new homepage (not quite right markup / style)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,8 @@
 class HomeController < ApplicationController
   def index
 
-    
-    
-    @events = Event.sorted_by_created_at.without_tagging_events.limit_to(5)
-    @latest_event = Event.sorted_by_created_at.first
-    @users = User.has_bio_and_avatar.recently_contributed(3)
     @tags = Tag.all_with_count :limit=>20
-    @groups = Group.all
-    
+    @latestFeature = PostImage.find(:all, :include => :post, :limit=>1)
     @featured = PostImage.find(:all, :include => :post, :limit=>50)
   end
 end
