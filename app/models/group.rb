6c3328d3e494d516268d5f69cc388dae353414c1
Tim Diggins <tim@red56.co.uk>
2009-03-07 21:08:38 +0000

    ensuring that rssfeeds are deleted when group deleted

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   validates_uniqueness_of   :name, :case_sensitive => false
   has_many :group_permissions, :include => :user, :uniq => true, :dependent=>:destroy
-  has_many :rss_feeds
+  has_many :rss_feeds, :dependent => :destroy
   has_many :posts, :dependent => :destroy
   has_many :post_images, :through => :posts, :order => "updated_at DESC" 
   
