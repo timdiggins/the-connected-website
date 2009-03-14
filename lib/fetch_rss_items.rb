5f65f6c0f10af4c72b956fa58f10e434e7c08eb0
Tim Diggins <tim@red56.co.uk>
2009-03-14 16:29:03 +0000

    improved readability of fetch_rss_items

--- lib/fetch_rss_items.rb
+++ lib/fetch_rss_items.rb
@@ -2,19 +2,20 @@ require File.dirname(__FILE__) + '/../app/models/rss_feed.rb'
 
 class FetchRssItems
   def self.fetch_one
+    #return whether worth checking again soon
     begin
       rss_feed = RssFeed.find_next_to_fetch
       rss_feed.check_feed()
-      return rss_feed
     rescue ActiveRecord::StaleObjectError
       #do nothing
     rescue ActiveRecord::RecordNotFound
+      #not worth checking again for a while
       return false
     rescue
       puts "error #{$!} %s" % rss_feed
       return unless rss_feed
       rss_feed.update_attributes(:error_message=> "Unexpected error: #{$!}", :next_fetch => Time.now + 1.minute)
-      return 
     end
+    return true
   end
 end
--- script/rss_feed_fetcher.rb
+++ script/rss_feed_fetcher.rb
@@ -5,8 +5,8 @@ require root + '/config/environment'
 require root + '/lib/fetch_rss_items'
 
 loop do
-  rss_feed = FetchRssItems.fetch_one
-  if rss_feed
+  worth_checking_again_soon = FetchRssItems.fetch_one
+  if worth_checking_again_soon
     #got one... sleep for a bit just not to be a processor hog
     puts "#{Time.now.to_s :short} checked #{rss_feed.url}"
     sleep 5
